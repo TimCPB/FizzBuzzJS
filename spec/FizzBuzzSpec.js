339ec14868ad2059fdd47760d3216d63e7e08c0a
Tim <TimCPB@github.com>
2020-09-01 21:49:26 +0100

    red test - 'returns 1 when given 1'

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,3 +1,11 @@
 describe("FizzBuzz", function() {
-  
+  var fizzBuzz;
+
+  // beforeEach(function() {
+  //   var fizzBuzz = new FizzBuzz();
+  // });
+
+  it("returns 1 when given 1", function(){
+    expect(fizzBuzz(1)).toEqual(1);
+  })
 });
